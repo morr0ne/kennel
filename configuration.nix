c82e399cd5286243e557ededd5eea50eefeb190d
Matilde Morrone <contact@morrone.dev>
2026-02-07 21:20:48 +0100

    Fully enable pipewire

--- configuration.nix
+++ configuration.nix
@@ -53,6 +53,10 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    jack.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    wireplumber.enable = true;
   };
 
   services.displayManager.ly.enable = true;
